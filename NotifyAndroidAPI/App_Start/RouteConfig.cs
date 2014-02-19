cf83e7822b8e79d7d6e480a2a6f93678e25eed85
pwhitdog <paul.whitmer@gmail.com>
2014-02-19 07:16:05 -0800

    Starting attempt for ajax html interface.

--- NotifyAndroidAPI/App_Start/RouteConfig.cs
+++ NotifyAndroidAPI/App_Start/RouteConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -18,6 +20,7 @@ namespace NotifyAndroidAPI
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
